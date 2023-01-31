56a8374f24c7359590d40719ed875cdcc133e0a5
WRWIII <wwaller@kent.edu>
2023-01-31 17:14:00 -0500

    adding react weather application

---
+++ react-weather-app/src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '6ad79bcffemsh7290e7087b4360ap1f1aa1jsn4935ce3c87e4',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "dc1747114f2f319336571a93718eed83";
